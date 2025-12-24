7fb206c7eed59558f023f44eae5744f5ccd4c4fc
PhamQuangAnh <anhpq.22ba13030@usth.edu.vn>
2025-12-24 16:21:15 +0700

    add about us

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Create map
+// Create map********
 const bounds = L.latLngBounds(L.latLng(-60, -180), L.latLng(85, 60));
 
 const map = L.map("map", {
@@ -33,7 +33,7 @@ fetch(jsonPath)
         alert("Can not read data from json file");
     });
 
-// Create point function
+// Create point function*********
 function renderPoints(data) {
     data.forEach((point) => {
         // Check the location is valid or not
@@ -46,7 +46,7 @@ function renderPoints(data) {
         // Decide class for animation
         const classNameStr = isEarthquake ? "pulse-red" : "pulse-green";
 
-        // 3. Create Marker
+        // Create Marker
         const marker = L.circleMarker([point.lat, point.lng], {
             radius: 10,
             className: classNameStr,
@@ -56,7 +56,7 @@ function renderPoints(data) {
             opacity: 1,
         }).addTo(map);
 
-        // 4. Create Popup
+        // Create Popup
         const popupContent = `
             <div style="font-family: sans-serif;">
                 <h4 style="margin: 0 0 5px 0;">${point.location}</h4>
@@ -73,3 +73,53 @@ function renderPoints(data) {
         marker.bindPopup(popupContent);
     });
 }
+
+// About us logic***********
+document.addEventListener("DOMContentLoaded", () => {
+    const track = document.getElementById("track");
+    const nextBtn = document.getElementById("nextBtn");
+    const prevBtn = document.getElementById("prevBtn");
+    const dots = document.querySelectorAll(".dot-item");
+
+    let currentIndex = 0;
+    const totalSlides = 3;
+
+    // Process track
+    function updateCarousel() {
+        const gap = 30;
+        const translateValue = `calc(-${currentIndex} * (100% + 30px))`;
+        track.style.transform = `translateX(${translateValue})`;
+
+        // Update dots
+        dots.forEach((d) => d.classList.remove("active"));
+        if (dots[currentIndex]) dots[currentIndex].classList.add("active");
+    }
+
+    // Next button
+    nextBtn.addEventListener("click", () => {
+        if (currentIndex < totalSlides - 1) {
+            currentIndex++;
+        } else {
+            currentIndex = 0; // Quay vòng về đầu
+        }
+        updateCarousel();
+    });
+
+    // Prev button
+    prevBtn.addEventListener("click", () => {
+        if (currentIndex > 0) {
+            currentIndex--;
+        } else {
+            currentIndex = totalSlides - 1;
+        }
+        updateCarousel();
+    });
+
+    // Click on dot
+    dots.forEach((dot) => {
+        dot.addEventListener("click", (e) => {
+            currentIndex = parseInt(e.target.dataset.index);
+            updateCarousel();
+        });
+    });
+});
